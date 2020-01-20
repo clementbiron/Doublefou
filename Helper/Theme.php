b91005e3784c110bde6f3ec608736f7f79129bef
Clément Biron <clement.biron@gmail.com>
2020-01-20 11:18:01 +0100

    remove feed link in header

--- Helper/Theme.php
+++ Helper/Theme.php
@@ -35,6 +35,8 @@
 			remove_action('wp_head', 'rest_output_link_wp_head');
 			remove_action('wp_head', 'wp_oembed_add_discovery_links');
 			remove_action('wp_head', 'wp_resource_hints', 2 );
+			remove_action('wp_head', 'feed_links_extra', 3);
+			remove_action('wp_head', 'feed_links', 2 );	
 		}
 
 		/**
